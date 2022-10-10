66f92fa7aa1e00c6d15d019416ce58620c76a390
1B34kz <114583644+KACPER2023@users.noreply.github.com>
2022-10-10 10:21:24 +0000

    zrobina zadanie od 1 do 4 z KartaPracy2

---
+++ KartaPracy2.py
@@ -0,0 +1,33 @@
+# # Zad 1
+# n=int(input())
+
+# if n % 3 == 0:
+#   print("TAK")
+# else:
+#   print("NIE")
+
+# Zad 2
+# a=int(input())
+
+# if a >=100 and a < 1000 and a % 17 == 0:
+#   print("TAK")
+# else:
+#   print("NIE")
+
+# Zad 3
+
+# wiek=int(input())
+
+# if wiek <= 18:
+#   print("TAK jesteś pełnoletni")
+# else:
+#   print("NIE jesteś pełnoletni")
+
+# Zad 4
+
+# LIMIT = 20
+#  waga = int(input())
+# if waga <= LIMIT:
+#   print("JEDŹ")
+# else:
+#   print("NIE JEDŹ")
