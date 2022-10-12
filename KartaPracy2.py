ebc2d43c6b877c7749492d0f9c6392e6d79f3cee
1B34kz <114583644+KACPER2023@users.noreply.github.com>
2022-10-12 12:15:31 +0000

    Dodano zadanie 5,6,7

--- KartaPracy2.py
+++ KartaPracy2.py
@@ -31,3 +31,32 @@
 #   print("JEDŹ")
 # else:
 #   print("NIE JEDŹ")
+
+# Zad 5
+# a = int(input())
+# b = int(input())
+# c = int(input())
+
+# if c>a and c<b or (c>b and c<a):
+#   print("TAK")
+# else:
+#   print("NIE")
+
+# Zad 6
+
+# a = int(input())
+# p = int(input())
+# if (a**p-a) % p==0:
+#   print("TAK, spełnia MTF")
+# else:
+#   print("NIE, spełnia MTF")
+
+# Zad 7
+
+# p = int(input())
+# k = int(input())
+# s = int(input())
+# if p + 3*s >= k:
+#   print("TAK")
+# else:
+#   print("NIE")
