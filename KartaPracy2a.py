808e2e312034dd1032b8b4bc29b7228f0b5827d3
1B34kz <114583644+KACPER2023@users.noreply.github.com>
2022-10-16 21:32:01 +0000

    Zrobiono wszystkie zadania z KartaPracy2a

---
+++ KartaPracy2a.py
@@ -0,0 +1,82 @@
+# Zad 1
+
+# a = int(input())
+# b = int(input())
+
+# if (a + b) % 2 == 0:
+#   print("TAK")
+# else:
+#   print("NIE")
+
+# Zad 2
+
+# a = int(input())
+# g = int(input())
+
+# if (a + g) / 2 > (a * g)**(1/2):
+#   print("TAK")
+# else:
+#   print("NIE")
+
+# Zad 3
+
+# k = int(input())
+# l = int(input())
+# m = int(input())
+
+# if k==l or k==m or m==l:
+#   if k==l:
+#     print("TAK")
+#     print(k,l)
+#   if k==m:
+#     print("TAK")
+#     print(k,m)
+#   if m==l:
+#     print("TAK")
+#     print(m,l)
+# else:
+#   print("NIE")
+
+# Zad 4
+
+# a = int(input())
+# b = int(input())
+# c = int(input())
+# d = int(input())
+# if a<b and a<c and a<d or b<a and b<c and b<d or c<a and c<b and c<d or d<a and d<b and d<c:
+#   if a<b and a<c and a<d:
+#     print(a)
+#   if b<a and b<c and b<d:
+#     print(b)
+#   if c<a and c<b and c<d:
+#     print(c)
+#   if d<a and d<b and d<c:
+#     print(d)
+# else:
+#   print("nie ma takiej liczby")
+
+# Zad 5
+
+# a = int(input())
+# b = int(input())
+# c = int(input())
+
+# if a < (b + c) and a > (b-c) and b < (a + c) and b > ( a - c) and c < (a + b) and c > (a - b):
+#   print("TAK")
+# else:
+#   print("NIE")
+
+# Zad 6
+# a = int(input())
+# b = int(input())
+# c = int(input())
+
+# if a < (b + c) and a > (b-c) and b < (a + c) and b > ( a - c) and c < (a + b) and c > (a - b):
+#   if a**2 + b**2 == c**2:
+#     print("trójkąt prostokątny")
+#   if a**2 + b**2 > c**2:
+#     print("trójkąt ostrokątny")
+#   if a**2 + b**2 < c**2:
+#     print("trójkąt rozwartokątny")
+# else:
+#   print("z tych liczb nie da się zbódować trójkąta")
