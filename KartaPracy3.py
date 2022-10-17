9f1b6e6255167e709d5b2aa10fd670710e6b5e45
1B34kz <114583644+KACPER2023@users.noreply.github.com>
2022-10-17 10:21:10 +0000

    Dodano zadania z pętli

---
+++ KartaPracy3.py
@@ -0,0 +1,22 @@
+# PRE
+
+# print(list(range(2, 8, 1)))
+
+# print(list(range(1, 8, 2)))
+
+# print(list(range(10)))
+
+# Zad pętla liczb dwucyfrowych od 10 do 21
+
+# for (i) in range(10,22):
+#   print(i)
+
+# Zad pętla liczb dwucyfrowych nieparzystych od 15 do 31
+
+# for (i) in range(15, 32, 2):
+#   print(i)
+
+# Zad pętla liczb dwucyfrowych malejąco (step ujemny)
+
+for (i) in range():
+  print(i)
