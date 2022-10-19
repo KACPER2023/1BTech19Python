a46429b9663346496714f05eca5f646a607df966
1B34kz <114583644+KACPER2023@users.noreply.github.com>
2022-10-19 12:16:22 +0000

    Dodano zadanie 1,2 z KartaPracy3

--- KartaPracy3.py
+++ KartaPracy3.py
@@ -18,5 +18,29 @@
 
 # Zad pętla liczb dwucyfrowych malejąco (step ujemny)
 
-for (i) in range():
-  print(i)
+# for (i) in range(99,9 -1):
+#   print(i, end=" ")
+
+# Zad pętla liczb dwucyfrowych malejąco(step dodatni)
+
+# for (i) in range(10, 100, 1):
+#   print(109-i, end=" ")
+
+# Zad pętla liczb 3-cyfrowych podzielna przez 20
+
+# for (i) in range(100, 981, 20):
+#   print(i, end=" ")
+
+# Zad 1
+
+# n = int(input())
+
+# for (i) in range(n):
+#   print(i**3 + 3, end=" ")
+
+# Zad 2
+
+# for (i) in range(105, 991, 15):
+#   print(i, end=" ")
+
+# Zad 3
