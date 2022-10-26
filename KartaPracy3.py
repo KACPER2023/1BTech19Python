4a6dceef53ac7a6623b32cc88701aeea5270f281
1B34kz <114583644+KACPER2023@users.noreply.github.com>
2022-10-26 12:16:59 +0000

    dodano zadania do KartaPracy3.py

--- KartaPracy3.py
+++ KartaPracy3.py
@@ -44,3 +44,34 @@
 #   print(i, end=" ")
 
 # Zad 3
+
+# p = int(input())
+
+# for (i) in range(1, p + 1):
+#   if p % i == 0:
+#     print(i)
+
+# Zad 4
+
+# a = 0
+
+# for i in range(10, 100):
+#   a = a + i 
+# print(a)
+
+# Zad 5
+
+# n = int(input("w ile gramy?"))
+
+# suma = n * (n+1)  // 2
+# for i in range(n-1):
+#   a = int(input())
+#   suma = suma - a
+# print("Brakuje:", suma)
+
+# Zad Dodatokowe - napisz pętle sumującą liczby dwucyfrowe parzyste
+
+# suma = 0
+# for i in range(10, 100, 2):
+#   suma = suma + n
+# print(suma)
