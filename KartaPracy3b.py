37ab3ebeee8a5318dd22df6eea9b0eb25a93c8df
1B34kz <114583644+KACPER2023@users.noreply.github.com>
2022-11-01 21:01:49 +0000

    dodano zadania od 1-9 z KartaPracy3b

---
+++ KartaPracy3b.py
@@ -0,0 +1,68 @@
+# Zad 1
+
+# for i in range(1, 31):
+#   print(i, end=" ")
+
+# Zad 2
+
+# for i in range(1, 100, 2):
+#   print(i**2, end=" ")
+
+# Zad 3
+
+# for i in range(1000, 9999):
+#   if i % 379 == 0:
+#     print(i, end=" ")
+
+# Zad 4
+
+# for i in range(100, 999):
+#   if i % 5 == 0 or i % 6 == 0 or i % 11 == 0:
+#     print(i, end=" ")
+
+# Zad 5
+
+# n = int(input("ile liczb chcesz wpisać ?"))
+# suma = 0
+# for i in range(n):
+#   a = int(input())
+#   suma += a
+#   print(suma)
+
+# Zad 6
+
+# k = int(input())
+# suma = 0
+# for i in range(0, k*2, 2):
+#   suma += i
+# print(suma, end=" ")
+
+# Zad 7
+
+# m = int(input())
+# suma = 0
+# for i in range(11, m*2+11, 2):
+#   suma += i
+# print(suma, end=" ")
+
+# Zad 8
+
+# W0 = int(input())
+# L = int(input())
+# a = 0
+# suma = W0
+# for i in range(0, L * 12):
+#   a = suma * 0.06 * (1/12)
+#   suma += a
+# print(suma, end=" ")
+
+# Zad 9
+
+# n = int(input())
+# suma = 0
+# for i in range(21, 1 + n, 100) :
+#   suma += i
+# print(suma, end=" ")
+
+# Zad 10
+
