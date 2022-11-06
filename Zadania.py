fcdcdd3e4185ebd6040e3cb6062958034e3ae07c
1B34kz <114583644+KACPER2023@users.noreply.github.com>
2022-11-06 19:01:58 +0000

    Dodano zadania od  1 - 4 w Zadania.py

---
+++ Zadania.py
@@ -0,0 +1,43 @@
+# Zadanie 1
+# Napisz program wyświetlający mniejszą z dwóch liczb podanych przez użytkownika. Jeżeli liczby są równe program wyświetla odpowieni komunikat.
+
+# a = int(input("Podaj liczbę:"))
+# b = int(input("Podaj liczbę:"))
+# if a < b:
+#   print(a)
+# elif a > b:
+#   print(b)
+# else:
+#   print("Liczby są sobe równe")
+
+# Zadanie 2
+# Napisz program informujący czy liczba podana przez użytkownika jest większa, mniejsza lub równa zero.
+
+# a = int(input("Podaj liczbę:"))
+# if a < 0:
+#   print("liczba jest mniejsza od zera")
+# elif a > 0:
+#   print("liczba jest większa od zera")
+# else:
+#   print("liczba jest równa zero")
+
+# Zadanie 3
+# Napisz program sprawdzający czy z boków o długościach podanych przez użytkownika można zbudować trójkąt.
+
+# a = int(input("Podaj wartość boku trójkąta:"))
+# b = int(input("Podaj wartość boku trójkąta:"))
+# c = int(input("Podaj wartość boku trójkąta:"))
+# if a + b >  c and a + c > b and  b + c > a:
+#   print("da się zbudowac trójkąta")
+# else:
+#   print("nie da się zbudowac trójkąta")
+
+# Zadanie 4
+# Napisz program, który dla zadanych dwóch liczb określi, czy pierwsza jest wielokrotnością drugiej.
+
+# a = int(input("Podaj liczbę:"))
+# b = int(input("Podaj liczbę:"))
+# if b % a == 0:
+#   print("liczba 1 jest wielokrotnością liczby drugiej")
+# else:
+#   print("liczba 1 niejest wielokrotnością liczby drugiej" )
