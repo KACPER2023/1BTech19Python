80094cf93f22fd4e405ad6c047a7af31061558f2
1B34kz <114583644+KACPER2023@users.noreply.github.com>
2022-12-12 11:21:40 +0000

    Dodana zadania do KartyPrac6.py

---
+++ Prace/KartaPracy6.py
@@ -0,0 +1,41 @@
+# Zad 1
+
+# a = int(input("Podaj liczbę"))
+# b = int(input("Podaj liczbę"))
+# c = int(input("Podaj liczbę"))
+
+# # arytmetyczne
+
+# if b - a == c - b: print("Arytmetyczne")
+
+# # geometryczne
+
+# if b / a == c / b: print("Geometryczne")
+
+# #rosnący 
+
+# if a < b < c: print("Rosnące") 
+
+# # malejące
+
+# if a > b > c: print("Malejące") 
+# Zad 2
+
+# suma = 0
+# for i in range(100, 1000):
+#  if i % 8 == 0 and i % 16 != 0:
+#     suma += i
+# print(suma)
+
+# Zad 3
+
+# ilosc = 0
+# for i in range(10,  100):
+#   cd = i // 10
+#   cj = i % 10
+#   if cd >= 2*cj:
+#     ilosc += 1
+# print(ilosc)
+  
+  
+  
